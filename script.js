1cc6ba28a475a3a9bf8d9e38bde705b4e9c76f60
jam5ghz <jodyarmstrong.prague@gmail.com>
2022-12-27 14:20:07 +0100

    ADDED logicin playRound function

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const computerPlayer = () =>{
-    const arrayRPS = ['Rock','Paper','scissors']
+const computerChoice = () =>{
+    const arrayRPS = ['rock','paper','scissors']
     const randNum = Math.floor(Math.random()*3)
-    console.log(arrayRPS[randNum])
+    const compChoice = arrayRPS[randNum]
+    return compChoice
 
 }
 
-computerPlayer()
+
+const playRound = (playerSelection, computerSelection) =>{
+    if (playerSelection === computerSelection){
+        return 'you tied ' + computerSelection + ' '+ playerSelection
+    } 
+    else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+        return 'you win ' + playerSelection +' beats ' + computerSelection
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        return 'you win ' + playerSelection +' beats ' + computerSelection
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        return 'you win ' + playerSelection +' beats ' + computerSelection
+    }
+    else {
+        return 'you lose' + computerSelection +' beats ' + playerSelection
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerChoice();
+console.log(playRound(playerSelection, computerSelection));
