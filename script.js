f1516987b5cd3fc90076633ee2f1eec0cfd4a96f
jam5ghz <jodyarmstrong.prague@gmail.com>
2022-12-27 15:14:05 +0100

    ADDED game function.  FIXED score iteration issue

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let compScore = 0
+
 const computerChoice = () =>{
     const arrayRPS = ['rock','paper','scissors']
     const randNum = Math.floor(Math.random()*3)
@@ -10,21 +13,45 @@ const computerChoice = () =>{
 const playRound = (playerSelection, computerSelection) =>{
     if (playerSelection === computerSelection){
         return 'you tied ' + computerSelection + ' '+ playerSelection
+        
     } 
     else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+        playerScore++
+
         return 'you win ' + playerSelection +' beats ' + computerSelection
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        playerScore++
         return 'you win ' + playerSelection +' beats ' + computerSelection
     }
     else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        playerScore++
         return 'you win ' + playerSelection +' beats ' + computerSelection
     }
     else {
-        return 'you lose' + computerSelection +' beats ' + playerSelection
+        compScore++
+        return 'you lose ' + computerSelection +' beats ' + playerSelection
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+const game = () =>{
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Enter your choice').toLowerCase()
+
+        const computerSelection = computerChoice();
+        console.log((playRound(playerSelection, computerSelection)));
+     }
+
+    if (playerScore === compScore){
+        return 'tied'
+
+    } 
+    else if (playerScore > compScore){
+        return 'you win'
+    }
+    else{
+        return 'you lose'
+    }
+}
+console.log(game())
