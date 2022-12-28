c90e92f696fa1a1091dd46ace5b78fce5e63ff1b
jam5ghz <jodyarmstrong.prague@gmail.com>
2022-12-28 14:59:05 +0100

    UPDATED computerChoice function

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 let playerScore = 0
 let compScore = 0
+const arrayRPS = ['rock','paper','scissors']
 
 const computerChoice = () =>{
-    const arrayRPS = ['rock','paper','scissors']
-    const randNum = Math.floor(Math.random()*3)
-    const compChoice = arrayRPS[randNum]
-    return compChoice
+    const randNum = Math.floor(Math.random() * arrayRPS.length)
+    return arrayRPS[randNum]
 
 }
 
 
 const playRound = (playerSelection, computerSelection) =>{
     if (playerSelection === computerSelection){
-        return 'you tied ' + computerSelection + ' '+ playerSelection
+        return 'you tied ' + computerSelection + ' cancels out '+ playerSelection
         
     } 
     else if (playerSelection === 'rock' && computerSelection === 'scissors'){
@@ -33,25 +32,22 @@ const playRound = (playerSelection, computerSelection) =>{
         return 'you lose ' + computerSelection +' beats ' + playerSelection
     }
 }
-
-
-const game = () =>{
+   
+const game= ()=>{
     for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Enter your choice').toLowerCase()
-
+        const playerSelection = prompt("enter choice").toLowerCase()
         const computerSelection = computerChoice();
-        console.log((playRound(playerSelection, computerSelection)));
+        console.log(playRound(playerSelection, computerSelection));
+     }
+     if (playerScore === compScore){
+        return 'tied'
+     } else if (playerScore > compScore){
+        return 'you win'
+     }else {
+        return 'you lose'
      }
 
-    if (playerScore === compScore){
-        return 'tied'
 
-    } 
-    else if (playerScore > compScore){
-        return 'you win'
-    }
-    else{
-        return 'you lose'
-    }
 }
+
 console.log(game())
